140df082c0046442d665595efe62ac2e634ef855
g-veronika <g.veronika2021@gmail.com>
2021-09-25 22:28:24 +0200

    add messages

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Run when client connects
 io.on('connection', socket => {
-    console.log('New WS Connection...');
 
-    socket.emit('message', 'Welcome to ChatCord !')
-})
+    //Welcome current user
+    socket.emit('message', 'Welcome to ChatCord !'); // To a single client
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has joined a chat'); //To all of the clients NOT connected
+
+    //Run when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+});
 
 const PORT = 3001 || process.env.PORT;
 
