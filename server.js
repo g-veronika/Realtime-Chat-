5dd81a74605a3026574b0e725d83e456be498955
g-veronika <g.veronika2021@gmail.com>
2021-09-25 22:15:13 +0200

    socket connexion

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
 
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const PORT = 3000 || process.env.PORT;
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...');
+
+    socket.emit('message', 'Welcome to ChatCord !')
+})
+
+const PORT = 3001 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
