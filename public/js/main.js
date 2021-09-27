5e7992a2b660f65c64f927f86f9b93a247bc5756
g-veronika <g.veronika2021@gmail.com>
2021-09-27 22:59:57 +0200

    bugs checked

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
 //Get username and room from URL
@@ -10,7 +11,7 @@ const {username, room} = Qs.parse(location.search, {
 const socket = io();
 
 //Join chatroom 
-socket.emit('joinRoom', {username, room});
+socket.emit('joinRoom', { username, room });
 
 //Get room and users 
 socket.on('roomUsers', ({ room, users }) => {
@@ -34,6 +35,12 @@ socket.on('message', (message) => {
 
   //Get message text
   let msg = e.target.elements.msg.value;
+
+  msg = msg.trim();
+
+  if (!msg) {
+    return false;
+  }
 
   //Emit message to server
   socket.emit('chatMessage', msg);
@@ -62,15 +69,26 @@ function outputMessage(message) {
 
 //Add room name to DOM 
 function outputRoomName(room) {
-  roomName.innerText = room
+  roomName.innerText = room;
 };
 
-//Add users name to DOM 
+// Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = `
-    ${users.map(user => `<li>${user.username}</li>`).join()}
-  `;
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
 };
 
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
 
 
