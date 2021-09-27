3c4fffbb9a10061b13b78f846904f795d9d73460
g-veronika <g.veronika2021@gmail.com>
2021-09-27 22:16:27 +0200

    user has left the chat

--- server.js
+++ server.js
@@ -44,7 +44,12 @@ io.on('connection', socket => {
 
     //Run when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+
+        const user = userLeave(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`))
+        }
     })
 
 
